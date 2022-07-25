9713db6a5265a37af7a5fc4c61f5ad6844fc6dbc
Larrire <linekerlarrire@gmail.com>
2022-07-25 09:33:01 -0300

    Criando npm script para iniciar o servidor de desenvolvimento

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (request, response) => {
-  response.send('hello dd!')
+  response.send('hello cc!')
 })
 
 app.listen(3030, () => console.log('Server started at 3030'))
