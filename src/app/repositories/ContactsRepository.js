e29b145775d43ee88b5ab0834e467e653efcf893
Larrire <linekerlarrire@gmail.com>
2022-08-15 20:57:23 -0300

    Implementando funcionalidades de buscar dados de um usuário e deletar usuário

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,11 +1,17 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Mateus',
     email: 'mateus@email.com',
     phone: '123123123',
+    category_id: v4(),
+  }, {
+    id: v4(),
+    name: 'José',
+    email: 'jose@email.com',
+    phone: '32321321',
     category_id: v4(),
   },
 ];
@@ -14,6 +20,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
